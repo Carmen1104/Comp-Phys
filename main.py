8f81f4fa233039cd782155ee094475c94414f25d
Carmen1104 <carmencongko11@gmail.com>
2023-06-19 11:40:13 +0700

    change

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ features = ['Safety Score', 'Days Since Inspection', 'Control Metric', 'Turbulen
 X=data.drop(['Severity', 'Accident_ID'],axis=1)
 y=data.Severity
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
 
-classifier = RandomForestClassifier()
+classifier = RandomForestClassifier(criterion='entropy',
+                             n_estimators=100,
+                             max_depth=10,)
 classifier.fit(X_train, y_train)
 
 window = tk.Tk()
